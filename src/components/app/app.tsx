1c00905f7a01fef19882d1eabe281881b2cae714
Radu <radu.schirliu@gmail.com>
2019-09-02 11:55:04 -0600

    Changed file structure to move App into it's own component

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import styles from './App.module.scss';
-import Navbar from './components/navbar/navbar';
+import styles from './app.module.scss';
+import Navbar from '../navbar/navbar';
 
 class App extends React.Component {
   render() {
     return (
       <div className={styles.container}>
-        <Navbar/>
+        <Navbar />
       </div>
     );
   }
