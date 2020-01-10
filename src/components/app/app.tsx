f3af6ae9cbe1a623e0d9b8c82df390fa3d2004ff
Radu Schirliu <radu.schirliu@gmail.com>
2020-01-10 16:53:28 -0700

    Fixed Material priamry color

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -5,24 +5,34 @@ import Navbar from '../navbar/navbar';
 import TodoCard from '../todo-card/todo-card';
 import NewTodoCard from '../new-todo-card/new-todo-card';
 import todoStore from '../../stores/todo-store';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 
 @observer
 class App extends React.Component {
   render() {
     let todos = todoStore.todos.slice().reverse();
+    let theme = createMuiTheme({
+      palette: {
+        primary: {
+          main: '#2D9CDB'
+        }
+      }
+    });
 
     return (
-      <div className={styles.container}>
-        <Navbar />
+      <ThemeProvider theme={theme}>
+        <div className={styles.container}>
+          <Navbar />
 
-        <div className={styles.cardContainer}>
-          <NewTodoCard />
-          
-          {todos.map(todo => (
-            <TodoCard key={todo.id} todo={todo}/>
-          ))}
+          <div className={styles.cardContainer}>
+            <NewTodoCard />
+            
+            {todos.map(todo => (
+              <TodoCard key={todo.id} todo={todo}/>
+            ))}
+          </div>
         </div>
-      </div>
+      </ThemeProvider>
     );
   }
 }
